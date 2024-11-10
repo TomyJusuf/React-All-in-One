2690cb92f9ec73301f2f55786a237ab7d6343101
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-11-10 18:28:44 +0100

    Use spread operator as argument for component Book

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -14,25 +14,9 @@ function BookList() {
   // Each Book component is passed a book object as a prop
   return (
     <section className="booklist">
-      <Book
-        author={books[0].author}
-        title={books[0].title}
-        image={books[0].img}
-      >
-        {/* here is the children */}
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
-          voluptatibus nobis cupiditate nihil rerum itaque voluptas dolore quos,
-          voluptatum atque consequuntur! Debitis, culpa. Mollitia nulla quos
-          quisquam velit repudiandae aperiam.
-        </p>
-        <button>Click me</button>
-      </Book>
-      <Book
-        author={books[1].author}
-        title={books[1].title}
-        image={books[1].img}
-      />
+      {books.map((book, index) => {
+        return <Book {...book} key={index} />
+      })}
     </section>
   )
 }
