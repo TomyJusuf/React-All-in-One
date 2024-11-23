b58f3d1c90ae46433d1bcdd67b2869c6b0551334
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-11-23 21:01:47 +0100

    useEffect ( basic - multiple effects - fetch data )

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,34 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((users) => setUsers(users))
+      .catch((error) => console.log(error))
+  }, [])
+
+  return (
+    <div>
+      <h2>github users</h2>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+export default FetchData
