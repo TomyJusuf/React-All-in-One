a98be106b06cc274d0e6c78be3f1545104235099
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-12-09 19:38:22 +0100

    Cleanup function and removeEventListener

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react'
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+  const [toggle, setToggle] = useState(false)
 
-export default CleanupFunction;
+  return (
+    <div>
+      <h2>cleanup function</h2>
+      <button type="button" className="btn" onClick={() => setToggle(!toggle)}>
+        {toggle ? 'hide' : 'show'}
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  )
+}
+
+const RandomComponent = () => {
+  useEffect(() => {
+    // const intID = setInterval(() => {
+    // console.log('Hello from setInterval')
+    // }, 1000)
+    // return () => clearInterval(intID)
+
+    const someFn = () => {
+      // some logic
+    }
+    window.addEventListener('scroll', someFn())
+    return () => window.removeEventListener('scroll', someFn())
+  }, [])
+  return <h1>hello there</h1>
+}
+
+export default CleanupFunction
